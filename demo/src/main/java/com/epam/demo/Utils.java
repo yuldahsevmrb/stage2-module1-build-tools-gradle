4f0246f4ee1b337a0e6a731c263151d51ccef9d6
узаффар лдашев <myuldashev@paymart.uz>
2023-04-11 13:36:02 +0500

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String arg : args) {
+            if (!arg.matches("\\d+")) { // checks if string is all digits
+                return false;
+            }
+            if (arg.startsWith("0")) { // checks if string starts with 0
+                return false;
+            }
+        }
+        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,19 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        if (StringUtils.isEmpty(str)) {
+            return false;
+        }
+        String clean = str.trim().replaceAll(",", "");
+        if (clean.charAt(0) == '-') {
+            return false;
+        }
+        if (clean.charAt(0) == '+') {
+            clean = clean.substring(1);
+        }
+        if (clean.matches("^[0-9]+(\\.[0-9]+)?$")) {
+            return true;
+        }
+        return false;
     }
 }
